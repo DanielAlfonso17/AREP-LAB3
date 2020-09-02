3a6ce850a7d566c209be5cf08e6f9e48dd653d99
DanielAlfonso17 <danielalfonso.29@hotmail.com>
2020-09-02 15:53:11 -0500

    Pantallazos lab

--- src/main/java/edu/escuelaing/arep/tallerservidorcliente/httpServer/FileB.java
+++ src/main/java/edu/escuelaing/arep/tallerservidorcliente/httpServer/FileB.java
@@ -5,7 +5,17 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
 
+/**
+ * interfaz encargada de generar la respuesta dependiendo del tipo de archivo
+ * css, js, html, png
+ */
 public interface FileB {
+    /**
+     *
+     * @param path ruta del archivo que queremos
+     * @param clientSocket socket del cliente para escribir la información solcitada
+     * @throws IOException
+     */
     public void getFile(String path, Socket clientSocket) throws IOException;
 
 }
